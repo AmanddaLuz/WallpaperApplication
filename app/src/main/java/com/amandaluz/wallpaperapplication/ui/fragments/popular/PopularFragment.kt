07b51e9bc8003ff4caa3e18f08b934d3665aafd7
amanddaluz <amandaluz@brq.com>
2023-03-13 12:03:41 -0300

    [Views] -  Criação do PhotoAdapter e do PhotoViewHolder

--- app/src/main/java/com/amandaluz/wallpaperapplication/ui/fragments/popular/PopularFragment.kt
+++ app/src/main/java/com/amandaluz/wallpaperapplication/ui/fragments/popular/PopularFragment.kt
@@ -5,22 +5,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.amandaluz.core.module.PhotoDomain
 import com.amandaluz.wallpaperapplication.databinding.FragmentPopularBinding
+import com.amandaluz.wallpaperapplication.ui.fragments.adapter.photoadapter.PhotoAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PopularFragment : Fragment() {
-    private lateinit var binding : FragmentPopularBinding
+    private lateinit var binding: FragmentPopularBinding
+    private lateinit var photoAdapter: PhotoAdapter
 
     override fun onCreateView(
-        inflater : LayoutInflater , container : ViewGroup? ,
-        savedInstanceState : Bundle?
-    ) : View {
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = FragmentPopularBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
-    override fun onViewCreated(view : View , savedInstanceState : Bundle?) {
-        super.onViewCreated(view , savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+    }
+
+    private fun initAdapter(){
+        photoAdapter = PhotoAdapter(::detail)
+        val gridLayoutManager = GridLayoutManager(requireContext(), 3)
+        with(binding.rvPopular){
+            scrollToPosition(0)
+            layoutManager = gridLayoutManager
+            setHasFixedSize(true)
+            adapter = photoAdapter
+        }
+    }
+    private fun detail(photoDomain: PhotoDomain){
+
     }
 }
